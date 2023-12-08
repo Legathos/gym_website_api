39667c1a742638b6123fd43475eb624c390586b1
Albert <albertcaprar@gmail.com>
2023-12-08 18:31:55 +0200

    Resolved some errors

--- src/main/java/config/SecurityConfigDefault.java
+++ src/main/java/config/SecurityConfigDefault.java
@@ -1,23 +1,21 @@
 package config;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.http.HttpMethod;
+ import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+ import org.springframework.security.web.SecurityFilterChain;
+ import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.http.HttpMethod;
-        import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-        import org.springframework.security.web.SecurityFilterChain;
-        import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-        import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+ import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfigDefault {
 
-    @Autowired
+
     private AuthEntryPointJwt unauthorizedHandler;
 
     @Bean

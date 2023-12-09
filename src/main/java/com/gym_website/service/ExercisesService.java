b85c1679e9c7f409cd9c7f650b69b59ae0989361
Albert <albertcaprar@gmail.com>
2023-12-10 01:05:54 +0200

    added edit and delete in exercise service

--- src/main/java/com/gym_website/service/ExercisesService.java
+++ src/main/java/com/gym_website/service/ExercisesService.java
@@ -1,8 +1,10 @@
 package com.gym_website.service;
 
 import com.gym_website.dto.ExercisesDto;
+import com.gym_website.dto.ResponseDto;
 import com.gym_website.entity.ExercisesEntity;
 import com.gym_website.mapper.ExercisesMapper;
+import org.apache.coyote.Response;
 import org.springframework.stereotype.Service;
 import com.gym_website.repository.ExercisesRepository;
 
@@ -29,6 +31,26 @@ public class ExercisesService {
         return  exercisesEntity.map(exercisesMapper::toDto).orElse(null);
     }
 
+    public ResponseDto deleteExercise(Long id){
+        ResponseDto responseDto = new ResponseDto();
+        if (exercisesRepository.findById(id).orElse(null)!=null){
+            exercisesRepository.delete(exercisesRepository.findById(id).orElse(null));
+            responseDto.setSuccessMessage("Exercise deleted");
+        }
+        responseDto.setErrorMessage("Exercise not found");
+        return  responseDto;
+    }
+
+    public ResponseDto editExercise(ExercisesDto exercisesDto){
+        ExercisesEntity exercisesEntity = exercisesRepository.findById(exercisesDto.getId()).orElse(null);
+        exercisesEntity.setName(exercisesDto.getName());
+        exercisesEntity.setCategory(exercisesDto.getCategory());
+        exercisesEntity.setDescription(exercisesDto.getDescription());
+        exercisesRepository.save(exercisesEntity);
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setSuccessMessage("Exercise modified");
+        return responseDto;
+    }
 
 
 }

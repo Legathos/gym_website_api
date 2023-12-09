f67489cc6050dbf213d5feac078b9739ad01b9f9
Albert <albertcaprar@gmail.com>
2023-12-09 09:29:17 +0200

    Nothing works security wise so after a mental breakdown we start from scratch

--- src/main/java/zGymProject/project/GymWebsiteApplication.java
+++ src/main/java/zGymProject/project/GymWebsiteApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class  GymWebsiteApplication {
 
 	public static void main(String[] args) {

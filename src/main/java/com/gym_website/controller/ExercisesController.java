1f71bc0b2b6deceb1355dd8ff266778a6ce5b21d
Albert <albertcaprar@gmail.com>
2023-12-10 13:25:49 +0200

    for exercises: added crud operation, one query in repo and one more mapper function

--- src/main/java/com/gym_website/controller/ExercisesController.java
+++ src/main/java/com/gym_website/controller/ExercisesController.java
@@ -1,7 +1,9 @@
 package com.gym_website.controller;
 
+import com.gym_website.dto.ExercisesDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.gym_website.service.ExercisesService;
 
 @Controller
@@ -11,6 +13,37 @@ public class ExercisesController {
     private final ExercisesService exercisesService;
     public ExercisesController(ExercisesService exercisesService){
         this.exercisesService = exercisesService;
+    }
+
+    @PostMapping("add-exercise")
+    public ResponseEntity addExercise (@RequestBody ExercisesDto exercisesDto){
+        return ResponseEntity.ok(exercisesService.addExercise(exercisesDto));
+    }
+
+    @GetMapping("get-exercise-{id}")
+    public ResponseEntity getExercise(@PathVariable("id")Long id){
+        return ResponseEntity.ok(exercisesService.getExercise(id));
+    }
+
+    @GetMapping("get-all-exercises")
+    public ResponseEntity getAllExercises(){
+        return ResponseEntity.ok(exercisesService.gelAllExercises());
+    }
+
+
+    @GetMapping("get-{category}-exercises")
+    public ResponseEntity getExercisesByCategory(@PathVariable("category")Long category){
+        return ResponseEntity.ok(exercisesService.gelExercisesByCategory(String.valueOf(category)));
+    }
+
+    @DeleteMapping("delete-exercise-{id}")
+    public ResponseEntity deleteById(@PathVariable("id")Long id){
+        return ResponseEntity.ok(exercisesService.deleteExercise(id));
+    }
+
+    @PutMapping("edit-exercise")
+    public ResponseEntity editExercise(@RequestBody ExercisesDto exercisesDto){
+        return ResponseEntity.ok(exercisesService.editExercise(exercisesDto));
     }
 
 
